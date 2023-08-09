985ec77dd0e7724bdc9113540f09f6dd0cf1a18a
Nagaraja.S <nagaraja.s@lynk.co.in>
2023-08-09 07:30:14 +0530

    MC-3 Alarm Local DB Store and Fetch

---
+++ app/src/main/java/com/geekydroid/materialclock/application/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.geekydroid.materialclock.application.di
+
+import android.content.Context
+import androidx.room.Room
+import com.geekydroid.materialclock.application.db.AppDatabase
+import com.geekydroid.materialclock.application.db.dao.AlarmDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+
+    @Singleton
+    @Provides
+    fun provideRoomDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, "mc_database").build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesAlarmDao(database: AppDatabase): AlarmDao {
+        return database.alarmDao()
+    }
+
+}
---
+++ app/src/main/java/com/geekydroid/materialclock/ui/alarm/model/AlarmMaster.kt
@@ -0,0 +1,32 @@
+package com.geekydroid.materialclock.ui.alarm.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.geekydroid.materialclock.ui.alarm.composables.AlarmScheduleType
+import com.geekydroid.materialclock.ui.alarm.composables.AlarmStatus
+
+@Entity(tableName = "MC_ALARM_MASTER")
+data class AlarmMaster(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo("alarm_id")
+    val alarmId: Int = 0,
+    @ColumnInfo("alarm_status")
+    val alarmStatus: AlarmStatus,
+    @ColumnInfo("alarm_label")
+    val alarmLabel: String,
+    @ColumnInfo("alarm_time_millis")
+    val alarmTimeInMillis: Long,
+    @ColumnInfo("alarm_date_in_millis")
+    val alarmDateInMillis: Long,
+    @ColumnInfo("alarm_scheduled_days")
+    val alarmScheduledDays: String,
+    @ColumnInfo("alarm_schedule_type")
+    val alarmType: AlarmScheduleType,
+    @ColumnInfo("is_alarm_vibrate")
+    val isAlarmVibrate: Boolean,
+    @ColumnInfo("created_on")
+    val createdOn: Long,
+    @ColumnInfo("updated_on")
+    val updatedOn: Long
+)
